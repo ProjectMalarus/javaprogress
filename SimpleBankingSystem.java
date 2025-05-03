a2db6ee1b32e8a8e85bb5198c903fd203fc47861
ProjectMalarus <160180916+ProjectMalarus@users.noreply.github.com>
2025-05-02 20:13:25 -0400

    Day 7 Project done, Simple Banking System

--- SimpleBankingSystem.java
+++ SimpleBankingSystem.java
@@ -1,10 +1,11 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 import java.util.UUID;
 
 public class SimpleBankingSystem {
-    class BankAccount{
+    public static class BankAccount{
         String ownerName;
-        int accountNumber;
+        BigInteger accountNumber;
         double balance;
 
         public BankAccount(String name){
@@ -13,14 +14,18 @@ public class SimpleBankingSystem {
             this.balance = 0.00;
         }
 
-        private static int generateID(){
-            int accountNumber = UUID.randomUUID().hashCode();
-            return accountNumber;
+        private static BigInteger generateID(){
+            BigInteger bigInteger = new BigInteger(UUID.randomUUID().toString().replace("-", ""), 16);
+            return bigInteger;
         }
 
         //Deposit method
         public void deposit(double x){
-            this.balance += x;
+            if(x <= 0.00){
+                System.out.println("Enter a valid amount please.");
+            } else{
+                this.balance += x;
+            }
         }
 
         //Withdraw method
@@ -35,18 +40,95 @@ public class SimpleBankingSystem {
 
         //Check balance
         public void checkBalance(){
-            System.out.println("Balance: " + this.balance);
+            System.out.printf("Balance: $%.2f%n", this.balance);
         }
 
         //Display account info
         public void displayAccInfo(){
             System.out.println("Name: " + this.ownerName);
             System.out.println("Account Number: " + this.accountNumber);
-            System.out.println("Balance: " + this.balance);
+            System.out.printf("Balance: $%.2f%n", this.balance);
         }
     }
 
     public static void main(String[] args){
 
+        boolean finished = false;
+        Scanner input = new Scanner(System.in);
+        BankAccount account = null;
+
+        do{
+            System.out.println("1) Create Account \n" +
+                                "2) Deposit \n" +
+                                "3) Withdrawal \n" + 
+                                "4) Check Balance \n" +
+                                "5) Display Account Information \n" + 
+                                "6) Exit");
+
+            int choice = input.nextInt();
+            input.nextLine();
+
+            switch(choice){
+                case 1:
+                    System.out.print("Enter a name for the account: ");
+                    String name = input.nextLine();
+                    account = new BankAccount(name);
+                break;
+
+                case 2:
+                    if (account == null) {
+                        System.out.println("Please create an account first.");
+                    break;
+                    }
+                
+                    System.out.print("Enter deposit amount: ");
+                    Double deposit = input.nextDouble();
+                    account.deposit(deposit);
+                    System.out.printf("New Balance: $%.2f%n", account.balance);
+                break;
+
+                case 3:
+                    if (account == null) {
+                        System.out.println("Please create an account first.");
+                    break;
+                    }
+                
+                    System.out.print("Enter withdrawal amount: ");
+                    Double withdrawal = input.nextDouble();
+                    account.withdraw(withdrawal);
+                    System.out.printf("New Balance: $%.2f%n", account.balance);
+                break;
+
+                case 4:
+                    if (account == null) {
+                        System.out.println("Please create an account first.");
+                    break;
+                    }
+                
+                    account.checkBalance();
+                break;
+
+                case 5:
+                    if (account == null) {
+                        System.out.println("Please create an account first.");
+                    break;
+                    }
+                
+                    account.displayAccInfo();
+                break;
+
+                case 6:
+                    System.out.println("Exiting program...");
+                    finished = true;
+                break;
+
+                default:
+                    System.out.println("Please enter a valid choice (1-6)");
+                break;
+            }
+            
+        }while(!finished);
+
+        input.close();
     }
 }
